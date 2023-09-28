bd6a8dfbd546d543b6bff4b866ccef065210fcc6
eric <ericibrahim19@gmail.com>
2023-09-28 19:15:57 -0300

    carpetas

---
+++ 1_unidad/Actividad 1/main.py
@@ -0,0 +1,9 @@
+# 1. Escribe un programa en Python que calcule el área de un triángulo 
+# dados la base y la altura.
+
+base = int(input('Ingrese el valor de la base: '))
+altura = int(input('Ingrese el valor de la altura: '))
+
+area_triangulo = base * altura / 2
+
+print('El área del triángulo es: ',area_triangulo)
---
+++ 1_unidad/Actividad 4/main.py
@@ -0,0 +1,32 @@
+#  4- Implementa una clase en Python que modele una cuenta bancaria, con métodos para depositar, 
+# retirar y consultar saldo.
+class cuenta_bancaria:
+    def __init__(self, saldo_inicial=0):
+        self.saldo = saldo_inicial
+
+    def depositar(self, cantidad):
+        if cantidad > 0:
+            self.saldo += cantidad
+            return f"Depósito exitoso. Saldo actual: ${self.saldo}"
+        else:
+            return "La cantidad a depositar debe ser mayor que cero."
+
+    def retirar(self, cantidad):
+        if cantidad > 0 and cantidad <= self.saldo:
+            self.saldo -= cantidad
+            return f"Retiro exitoso. Saldo actual: ${self.saldo}"
+        elif cantidad > self.saldo:
+            return "Fondos insuficientes para realizar el retiro."
+        else:
+            return "La cantidad a retirar debe ser mayor que cero."
+
+    def consultar_saldo(self):
+        return f"Saldo actual: ${self.saldo}"
+
+# Ejemplo de uso
+saldo = int(input('Saldo actual: ')) #Ingresa el saldo en la cuenta
+cuenta = cuenta_bancaria(saldo)  # Crear una cuenta con saldo inicial
+print(cuenta.consultar_saldo())  # Consultar saldo
+print(cuenta.depositar(500))  # Depositar $500
+print(cuenta.retirar(200))  # Retirar $200
+print(cuenta.consultar_saldo())  # Consultar saldo nuevamente
